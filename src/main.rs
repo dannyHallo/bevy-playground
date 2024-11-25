2c0b6bcee065a9fb681f55c767f122216b4c28df
danny <ruitian0716@gmail.com>
2024-11-26 02:17:13 +0800

    2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Danny".to_string())));
+    commands.spawn((Person, Name("Cecilia".to_string())));
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+    app.add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
+        .run();
 }
